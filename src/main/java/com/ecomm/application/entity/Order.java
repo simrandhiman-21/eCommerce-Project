653042575637f520093f4550b64e2914742373f1
simrandhiman <simrandhiman3121@gmail.com>
2026-09-08 10:18:52 +0530

    add: placing order

--- src/main/java/com/ecomm/application/entity/Order.java
+++ src/main/java/com/ecomm/application/entity/Order.java
@@ -1,6 +1,7 @@
 package com.ecomm.application.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Entity
 @NoArgsConstructor
+@Data
 @Table(name="orders")
 public class Order {
 
